e32d045aa2efdd32009442eb189182b75f7a1e8f
JulianAtencio <juat699@gmail.com>
2013-05-15 10:15:28 -0500

    Subo al Head

---
+++ src/MainHead.java
@@ -0,0 +1,11 @@
+
+public class MainHead {
+	 public static void main(String[] parametro) {
+	        System.out
+	                .println("=============Aja Cambio de Julian Repo Programa de MODIFICADO DOS prueba para GIT CON hub============");
+	        ImprimirHead imprimeHead = new ImprimirHead();
+	        //imprimeHead.devuelve_mensajeHead(true);
+	        imprimeHead.devuelve_mensajeHead(false);
+	    }
+
+}
